4deeaeb093eb6b15844740210c529c2e7d474733
bharaniy <54335412+bharaniy@users.noreply.github.com>
2019-08-29 23:48:26 -0500

    initial changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CurrentComponent} from './current/current.component';
+import {ForecastComponent} from './forecast/forecast.component';
 
-
-const routes: Routes = [];
+const WEATHER_ROUTER: Routes = [{path: '', component: CurrentComponent}, {path: 'forecast', component: ForecastComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(WEATHER_ROUTER)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const weatherRouting: ModuleWithProviders = RouterModule.forRoot(WEATHER_ROUTER);
